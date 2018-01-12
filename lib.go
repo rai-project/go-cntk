ccac7c5932f394d5ca00b79ac25bd966dbc117f3
Cheng Li <cli99@illinois.edu>
2018-01-12 18:28:09 +0800

    update dockerfhiles

--- lib.go
+++ lib.go
@@ -7,9 +7,9 @@ package cntk
 // #cgo LDFLAGS: -lCntk.PerformanceProfiler-2.3
 // #cgo LDFLAGS: -lCntk.Eval-2.3
 // #cgo LDFLAGS: -lmklml_intel -liomp5 -L${SRCDIR} -lstdc++
-// #cgo LDFLAGS: -L/opt/frameworks/cntk/cntk/lib -L/opt/frameworks/cntk/cntk/dependencies/lib
+// #cgo LDFLAGS: -L/opt/cntk/cntk/lib -L/opt/cntk/cntk/dependencies/lib -L/opt/frameworks/cntk/cntk/lib -L/opt/frameworks/cntk/cntk/dependencies/lib
 // #cgo CXXFLAGS: -std=c++11 -O3 -Wall -g
 // #cgo CXXFLAGS: -Wno-sign-compare -Wno-unused-function -Wno-reorder -Wno-unknown-pragmas
-// #cgo CXXFLAGS: -I/usr/local/cuda/include -I/opt/frameworks/cntk/Include
+// #cgo CXXFLAGS: -I/usr/local/cuda/include -I/opt/cntk/Include -I/opt/frameworks/cntk/Include
 // #cgo CXXFLAGS: -I${SRCDIR}/cbits -I${SRCDIR}/cbits/util -I${SRCDIR}/cuda
 import "C"
