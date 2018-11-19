611c28f356eadf66f0002387b83435a2761839a0
Cheng Li <cli99@illinois.edu>
2018-11-18 23:53:44 -0600

    add nogpu build tag

--- lib.go
+++ lib.go
@@ -2,15 +2,11 @@
 
 package cntk
 
-// #cgo LDFLAGS: -lcublas -lcudart -lcudnn -lcurand -lcusparse
-// #cgo LDFLAGS: -lCntk.Core-2.6
-// #cgo LDFLAGS: -lCntk.Math-2.6
-// #cgo LDFLAGS: -lCntk.PerformanceProfiler-2.6
-// #cgo LDFLAGS: -lCntk.Eval-2.6
-// #cgo LDFLAGS: -lmklml_intel -liomp5 -L${SRCDIR} -lstdc++
-// #cgo LDFLAGS: -L/opt/cntk/cntk/lib -L/opt/cntk/cntk/dependencies/lib -L/usr/local/cuda/lib64
-// #cgo CXXFLAGS: -std=c++11 -O3 -Wall -g
+// #cgo CXXFLAGS: -I${SRCDIR}/cbits -std=c++11 -O3 -Wall -g
 // #cgo CXXFLAGS: -Wno-sign-compare -Wno-unused-function -Wno-reorder -Wno-unknown-pragmas
-// #cgo CXXFLAGS: -I/usr/local/cuda/include -I/opt/cntk/Include
-// #cgo CXXFLAGS: -I${SRCDIR}/cbits -I${SRCDIR}/cbits/util -I${SRCDIR}/cuda
+// #cgo LDFLAGS: -lmklml_intel -liomp5 -L${SRCDIR} -lstdc++
+// #cgo LDFLAGS: -L/opt/cntk/cntk/lib -L/opt/cntk/cntk/dependencies/lib
+// #cgo LDFLAGS: -lCntk.Core-2.6 -lCntk.Math-2.6 -lCntk.PerformanceProfiler-2.6 -lCntk.Eval-2.6
+// #cgo !nogpu CXXFLAGS: -I/usr/local/cuda/include -I/opt/cntk/Include
+// #cgo !nogpu LDFLAGS: -lcublas -lcudart -lcudnn -lcurand -lcusparse -L/usr/local/cuda/lib64
 import "C"
